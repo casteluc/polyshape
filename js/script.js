e74d5594fa002830fdca8d5378aa48db3fe6c63a
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-02 01:47:25 -0300

    Moving dot on canvas

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-// Apply changes on window load
-window.addEventListener("load", () => {
-    // Getting the canvas element on window load
-    const canvas = document.getElementById("canvas")
-    const context = canvas.getContext('2d')
-    
-    // Adjusting canvas size
-    resizeCanvas()
-})
+// Getting the canvas and context elements
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+var dot = [10, 10]
+
+function main() {
+    draw()
+    setInterval(move, 50)
+}
 
 // Adjusts the canvas size according to window size
 function resizeCanvas() {
     canvas.height = window.innerHeight
     canvas.width = window.innerWidth
 }
+
+function draw() {
+    ctx.fillRect(dot[0],dot[1],3,3)
+}
+
+function move() {
+    ctx.clearRect(dot[0], dot[1], 3, 3)
+    dot[0] += 1
+    console.log(dot)
+    draw(ctx)
+}
+
+
+// Resizing canvas on window load
+window.addEventListener("load", resizeCanvas)
+
+// Executes main function
+main()
