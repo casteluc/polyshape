d21f05aa21b79cc3f23d5db778af86d801b4353c
Lucas Torres <lucasrib.ia@gmail.com>
2020-09-02 02:41:30 -0300

    Moving dots randomly on canvas

--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,27 @@
-import { randomInt } from './utils.js'
+import { randomInt, randomSpeed } from './utils.js'
 
 // Getting the canvas and context elements
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+// Initializing the dots list
 var dots = []
 
 // Creates all the inital dots that will be rendered on canvas
 function createDots(n) {
     for (let i = 0; i < n; i++) {
-        let newDot = [randomInt(0, canvas.width), randomInt(0, canvas.height)]
+        let newDot = {
+            pos: {
+                x: randomInt(0, canvas.width),
+                y: randomInt(0, canvas.height)
+            },
+
+            speed: {
+                x: randomSpeed(),
+                y: randomSpeed()
+            }
+        }
+
         dots.push(newDot)
     }
 }
@@ -17,15 +29,17 @@ function createDots(n) {
 // Renders all the dots on the canvas
 function drawDots() {
     dots.forEach( dot => {
-        ctx.fillRect(dot[0], dot[1], 3, 3)
+        ctx.fillRect(dot.pos.x, dot.pos.y, 3, 3)
     })
 }
+
 
 // Moves all the dots on canvas
 function moveDots() {
     dots.forEach( dot => {
-        ctx.clearRect(dot[0], dot[1], 3, 3)
-        dot[0] += 1
+        ctx.clearRect(dot.pos.x, dot.pos.y, 3, 3)
+        dot.pos.x += dot.speed.x
+        dot.pos.y += dot.speed.y
     })
     
     drawDots()
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,13 @@
 // Returns a random number between the 2 numbers given
 function randomInt(min, max) {
-    console.log("teste")
     return min + Math.floor((max - min) * Math.random());
 }
 
-export { randomInt }
+function randomSpeed() { 
+    return randomInt(-3, 3) 
+}
+
+export {
+    randomInt,
+    randomSpeed,
+}
